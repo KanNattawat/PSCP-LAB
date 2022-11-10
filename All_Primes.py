0d8a8015ffa5a5106d2987d667f0ac6d2e97b943
KanNattawat <nattawat26907@gmail.com>
2022-11-10 13:33:22 +0700

    commit

--- All_Primes.py
+++ All_Primes.py
@@ -1,12 +1,20 @@
-"""pscp"""
+""" PSCP """
 
-def isprime(num):
-    """isprime function"""
-    con = 0
-    if num > 1:
-        for i in range(1, num+1):
-            if i % i == 0 and i % 1 == 0:
-                con += 1
-    print(con)
 
-isprime(int(input()))
+def main(num):
+    """ main """
+    ans = 0
+
+    for i in range(1, num+1):
+        can_divide = 0
+        for val in range(1, i+1):
+            if i % val == 0:
+                can_divide += 1
+
+        if can_divide == 2:
+            ans += 1
+
+    print(ans)
+
+
+main(int(input()))
