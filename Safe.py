c21f4b904f75f984fdb28e3667e4519e9782da66
KanNattawat <nattawat26907@gmail.com>
2022-11-11 15:51:53 +0700

    modified

--- Safe.py
+++ Safe.py
@@ -1,22 +1,24 @@
 """ pscp """
 
+
 def main(str1, str2):
     """ main """
     ans = 0
-    abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     for i in range(len(str1)):
+        # print(str1[i], str2[i])
         if str1[i] == str2[i]:
             continue
-        dex_str1 = abc.find(str1[i])
-        dex_str2 = abc.find(str2[i])
-        print(dex_str1)
-        print(dex_str2)
-        if abs(dex_str1 - dex_str2) < dex_str1 - abc[26:].find(str2[i]) + 27:
-            ans += abs(dex_str1 - dex_str2)
-        else:
-            ans += dex_str1 - abc[26:].find(str2[i]) + 1
+        lst = []
+        dex_str1 = abc.index(str1[i])
+        dex_str2 = abc.index(str2[i])
+        way1 = abs(dex_str1 - dex_str2)
+        way2 = (abc[dex_str1::-1] + abc[-1:dex_str1:-1]).index(str2[i])
+        way3 = (abc[dex_str1:] + abc[:dex_str2+1]).index(str2[i])
+        lst.extend([way1, way2, way3])
+        ans += min(lst)
 
     print(ans)
-    print(dex_str1 - abc[26:].find(str2[i]))
+
 
 main(input(), input())
